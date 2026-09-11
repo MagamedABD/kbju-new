00132b420820744d06871fc6d2babee50c51303e
MagamedABD <abdullaevmagamed16@gmail.com>
2026-09-11 20:39:52 +0300

    remove duplicate files from root

--- cn.ts
+++
@@ -1,4 +0,0 @@
-/** Простой конкатенатор классов: отбрасывает falsy-значения. */
-export function cn(...classes: Array<string | false | null | undefined>): string {
-  return classes.filter(Boolean).join(' ');
-}
